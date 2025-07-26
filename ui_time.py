a76bd327a8b58deebcf80ba95056faac6dce1b5c
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-26 08:31:43 +0300

    Update ui_time.py

--- ui_time.py
+++ ui_time.py
@@ -1,15 +1,27 @@
 import streamlit as st
 
-def render_time(time_now):
+def render_time(time_now, today_name):
     st.markdown(f'''
         <div style="
             width: 100%;
             text-align: center;
-            font-size: 28px;
-            font-weight: 700;
-            direction: ltr;
             margin-bottom: 30px;
         ">
-            ⏰ {time_now}
+            <div style="
+                font-size: 48px;
+                font-weight: 700;
+                direction: ltr;
+                margin-bottom: 5px;
+            ">
+                ⏰ {time_now}
+            </div>
+            <div style="
+                font-size: 28px;
+                color: #444;
+                font-weight: 600;
+                direction: rtl;
+            ">
+                {today_name}
+            </div>
         </div>
     ''', unsafe_allow_html=True)
