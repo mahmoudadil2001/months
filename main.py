7c4483fb40e0c6e4087128d9977e37d4ce0dc40e
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-26 09:23:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         for w in range(weeks):
             start_day = 7 + w * 7
             end_day = min(start_day + 7, total_days)
-            st.markdown(f"<div style='font-weight: 700; margin-top: 10px; border-top: 2px solid #888;'>الأسبوع {w + 1}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='font-weight: 700; margin-top: 10px; border-bottom: 2px solid #888;'>الأسبوع {w + 1}</div>", unsafe_allow_html=True)
             for i in range(start_day, end_day):
                 day_date = now + timedelta(days=i)
                 day_name = days_ar[day_date.weekday()]
