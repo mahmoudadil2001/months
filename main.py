a9c3c49f2f86cead31adf7b2f62fcecda357ab63
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-26 11:10:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
         """
         <style>
         section[data-testid="stSidebar"] div[data-baseweb="select"] > div {
-    min-width: 240px !important;
+    min-width: 270px !important;
     min-height: 60px !important;      /* هذا هو الحد الأدنى للارتفاع */
     background-color: #e6f0ff !important;
     border-radius: 8px !important;
